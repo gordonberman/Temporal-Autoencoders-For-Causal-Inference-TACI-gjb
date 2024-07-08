8bd4808ec6e95442cde980280a6a0f0fa77e7deb
Gordon Berman <gordon.berman@gmail.com>
2024-07-08 16:26:46 -0400

    Update Henon_pulses.py
    bug fix

--- data/dynamic_synthetic/Henon_pulses.py
+++ data/dynamic_synthetic/Henon_pulses.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import signal
+from numba import njit
 
 ################################################################
 ############## Henon-Henon Temporal Attractors  ################
